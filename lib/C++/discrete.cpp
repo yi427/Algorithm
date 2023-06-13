32c4831f85ffe31d5f663b401a1efb743402cb53
yi427 <109063757+yi427@users.noreply.github.com>
2023-06-13 21:05:30 +0800

    Update discrete.cpp
    add the compare functional

--- lib/C++/discrete.cpp
+++ lib/C++/discrete.cpp
@@ -1,11 +1,17 @@
+#include <vector>
+#include <iostream>
+#include <functional>
 
-// finished time: 2023.6.9
+// start time: 2023.6.9
 // author: pan__
+using namespace std;
+
 template<typename T>
 class discrete {
 public:
-    explicit discrete(const vector<T> &p) : arr(p) {
-        sort(begin(arr), end(arr));
+    template<class _f = function<bool(T, T)>>
+    explicit discrete(const vector<T> &p, _f com = std::less<T>()) : arr(p) {
+        sort(begin(arr), end(arr), com);
         arr.resize(unique(begin(arr), end(arr)) - begin(arr));
         this->length = size(arr);
     }
@@ -28,3 +34,14 @@ private:
     vector<T> arr;
     int length;
 };
+
+int main()
+{
+    // [[nodiscard]] 的意思是这个值不允许被丢弃，换句话说，当你调用这个函数的时候，你肯定需要用到返回值，如果你没有用到，肯定是哪里写错了。
+    vector v{100, 900, 1000, 10};
+    // default compare is the std::less<T>()
+    discrete d(v, [](auto x, auto y) { return x > y; });
+    for (int i = 0; i < d.len(); ++i)
+        cout << d.get(i) << ' ';
+    return 0;
+}
