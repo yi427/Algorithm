11030a9676b70f90beda30077b9678791db43a3c
yi427 <109063757+yi427@users.noreply.github.com>
2023-06-15 20:45:08 +0800

    Update discrete.cpp
    为 unique 增加的lambda函数，并且修改了之前的一处错误

--- lib/C++/discrete.cpp
+++ lib/C++/discrete.cpp
@@ -6,19 +6,19 @@
 // author: pan__
 using namespace std;
 
-template<typename T>
+template<typename T, class _f1 = function<bool(T, T)>>
 class discrete {
 public:
-    template<class _f = function<bool(T, T)>>
-    explicit discrete(const vector<T> &p, _f com = std::less<T>()) : arr(p) {
+    template<class _f2 = _f1>
+    explicit discrete(const vector<T> &p, _f1 _com = std::less<T>(), _f2 eq = std::equal_to<T>()) : arr(p), com(_com) {
         sort(begin(arr), end(arr), com);
-        arr.resize(unique(begin(arr), end(arr)) - begin(arr));
+        arr.resize(unique(begin(arr), end(arr), eq) - begin(arr));
         this->length = size(arr);
     }
 
-    [[nodiscard]] int pos(T val) const { return lower_bound(begin(arr), end(arr), val) - begin(arr); }
+    [[nodiscard]] int pos(T val) const { return lower_bound(begin(arr), end(arr), val, com) - begin(arr); }
 
-    [[nodiscard]] int greater(T val) const { return lower_bound(begin(arr), end(arr), val + 1) - begin(arr); }
+    [[nodiscard]] int greater(T val) const { return lower_bound(begin(arr), end(arr), val + 1, com) - begin(arr); }
 
     [[nodiscard]] int greater_equal(T val) const { return pos(val); }
 
@@ -33,14 +33,17 @@ public:
 private:
     vector<T> arr;
     int length;
+    _f1 com;
 };
 
 int main()
 {
     // [[nodiscard]] 的意思是这个值不允许被丢弃，换句话说，当你调用这个函数的时候，你肯定需要用到返回值，如果你没有用到，肯定是哪里写错了。
+    // 注意，关于 逻辑操作符 只有在 升序 的情况下是正确的，其他情况下，结果是不可靠的。
     vector v{100, 900, 1000, 10};
     // default compare is the std::less<T>()
-    discrete d(v, [](auto x, auto y) { return x > y; });
+    discrete d(v, [](auto x, auto y) { return x > y; },
+               [](auto x, auto y) { return x == y; });
     for (int i = 0; i < d.len(); ++i)
         cout << d.get(i) << ' ';
     return 0;
