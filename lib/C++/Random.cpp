336c0df1dac6f4b0255bb63eb5bc6314a2357d57
yi427 <109063757+yi427@users.noreply.github.com>
2023-06-20 21:27:15 +0800

    Add files via upload

---
+++ lib/C++/Random.cpp
@@ -0,0 +1,21 @@
+#include <random>
+
+using namespace std;
+// 摘录 深度探索 C++ 14
+std::default_random_engine& global_urng()
+{
+    static std::default_random_engine u{};
+    return u;
+}
+
+void randomize()
+{
+    static std::random_device rd{};
+    global_urng().seed(rd());
+}
+
+int pick(int from, int to) {
+    static std::uniform_int_distribution<> d{};
+    using parm_t = decltype(d)::param_type;
+    return d(global_urng(), parm_t{from, to});
+}
